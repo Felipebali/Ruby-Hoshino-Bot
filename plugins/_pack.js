3d40bb0c60833535d5c7dc7d0bda2239c7d13d9d
Felipebali <felipebaliski@gmail.com>
2025-11-13 22:36:33 -0300

    Update _pack.js

--- plugins/_pack.js
+++ plugins/_pack.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 const handler = async (m, { conn }) => {
    if (!db.data.chats[m.chat].nsfw && m.isGroup) {
-    return m.reply(`�m?�nEl contenido *NSFW* est? desactivado en este grupo.\n> Un administrador puede activarlo con el comando ? *#nsfw on*`);
+    return m.reply(`El contenido *NSFW* está desactivado en este grupo.\n> Un owner puede activarlo usando *.nsfw*`);
     }
   try {
     const res = await fetch('https://api.waifu.pics/nsfw/waifu')
